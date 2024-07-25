677ad7756d3b545f2067e7d6b51f213d5d05345d
Sam Kamble <kamble.samarth91@gmail.com>
2024-07-26 02:38:06 +0530

    Implement the file and folder struture

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-3xl font-bold text-indigo-700">
+        This is a protected route
+      </p>
+    </div>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import "./globals.css";
+
+const font = Open_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Discord",
+  description:
+    "Discord is a voice, video and text communication service to talk and hang out with your friends and communities.",
+  icons: {
+    icon: "./logo.png ",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
